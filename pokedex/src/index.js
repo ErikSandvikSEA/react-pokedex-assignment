d7362d52b84a1f73008fff0a63696d589773050e
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-10-28 15:01:26 -0700

    setting up redux store, actions, reducers

--- pokedex/src/index.js
+++ pokedex/src/index.js
@@ -1,17 +1,26 @@
+// from packages
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+
+// from files 
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { reducer } from './state/reducers'
+
+const store = createStore(reducer, applyMiddleware(
+  thunk,
+  logger
+))
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ pokedex/src/state/reducers.js
@@ -0,0 +1,12 @@
+//import actions
+import {} from './actions'
+
+//initialState
+const initialState = {}
+
+export const reducer = (state = initialState, action) => {
+    switch(action.type) {
+         default:
+              return state
+    }
+}
